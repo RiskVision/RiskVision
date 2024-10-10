b632a305b5f66c19b7502f9e5d7f38bde601d556
J. Lugo <a01571142@tec.mx>
2024-10-10 16:01:34 -0600

    moved bakend

--- Test-API/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-const controller = require('../controllers/login')
-
-router.post('/',controller.login)
-router.post('/register', controller.addUser)
-
-module.exports = router
