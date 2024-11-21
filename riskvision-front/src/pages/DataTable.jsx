3e172eeb43dcb3024e4f6cdadabef80dbd012888
DiegoRdzD <91581962+DiegoRdzD@users.noreply.github.com>
2024-11-21 15:31:43 -0600

    Ajustrar cambio a BurgerMenu

--- riskvision-front/src/pages/DataTable.jsx
+++ riskvision-front/src/pages/DataTable.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import BurgerMenu from '../components/menu/burgerMenu';
+import BurgerMenu from '../components/Menu/BurgerMenu';
 
 const DataTables = () => {
     const [data, setData] = useState([]);
