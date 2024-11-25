20315f854f4b44521295a801f921b42917dc3afe
Elias Serrano <eliasjosf26@gmail.com>
2024-11-25 00:12:40 -0600

    login simplified

--- riskvision-front/src/pages/Inicio de sesion/login.jsx
+++ riskvision-front/src/pages/Inicio de sesion/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import rvlogo from '../../components/images/logo-white.png';
+import rvlogo from '../../components/images/logo-blue.png';
 import { useNavigate } from 'react-router-dom';
 import './login.css'; // Importar la clase CSS
 
@@ -38,17 +38,16 @@ const Login = () => {
 
   return (
     <div className='bg-banner'>
-      <div className="bg-overlay"></div> {/* Overlay con opacidad */}
-      <div className="flex items-center justify-center min-h-screen text-white relative z-10">
-        <div className="backdrop-blur-md bg-gray-800/80 p-6 rounded-lg w-96 shadow-lg ring-black/5"> {/* Cambiar el fondo a gris oscuro con opacidad */}
+      <div className="flex items-center justify-center min-h-screen text-gray-800 relative z-10">
+        <div className="backdrop-blur-md bg-white p-6 rounded-lg w-96 shadow-lg ring-black/5"> {/* Cambiar el fondo a blanco */}
           <div className='flex items-center justify-center mb-4'>
-            <img src={rvlogo} alt='logo' width={100}/>
+            <img src={rvlogo} alt='logo' width={120}/>
           </div>
-          <h2 className="text-2xl font-bold mb-4 text-center text-white">Iniciar Sesión</h2> {/* Cambiar el color del texto a blanco */}
+          <h2 className="text-2xl font-bold mb-4 text-center text-gray-800">Iniciar Sesión</h2> {/* Cambiar el color del texto a gris oscuro */}
           {error && <p className="text-red-500 text-sm">{error}</p>}
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
-              <label className="block text-white" htmlFor="user">Usuario</label> {/* Cambiar el color del texto a blanco */}
+              <label className="block text-gray-800" htmlFor="user">Usuario</label> {/* Cambiar el color del texto a gris oscuro */}
               <input
                 type="text"
                 id="user"
@@ -60,7 +59,7 @@ const Login = () => {
               />
             </div>
             <div className="mb-4">
-              <label className="block text-white" htmlFor="password">Contraseña</label> {/* Cambiar el color del texto a blanco */}
+              <label className="block text-gray-800" htmlFor="password">Contraseña</label> {/* Cambiar el color del texto a gris oscuro */}
               <input
                 type="password"
                 id="password"
@@ -73,7 +72,7 @@ const Login = () => {
             </div>
             <button
               type="submit"
-              className={`backdrop-blur-md w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 ${loading ? 'cursor-not-allowed' : ''}`}
+              className={`backdrop-blur-md w-full bg-blue-950 text-white py-2 rounded-md hover:bg-blue-700 ${loading ? 'cursor-not-allowed' : ''}`}
               disabled={loading} // Disable button while loading
             >
               {loading ? 'Cargando...' : 'Iniciar Sesión'} {/* Show loading text */}
